71b2128ec92efced3234ac83ebda338b518fd6df
Anny17f <annyfaruq17@gmail.com>
2024-06-14 10:26:39 +0100

    Initial commit

--- formatName.js
+++ formatName.js
@@ -1,7 +1,24 @@
-// formatName.js
-
 function formatName(name) {
-  return name.trim().replace(/\b\w/g, (char) => char.toUpperCase());
+  let trimmedName = name.trim();
+
+  let nameParts = trimmedName.split(' ');
+
+let formattedParts = [];
+
+for (let i = 0; i < nameParts.length; i++) {
+
+    let part = nameParts[i];
+
+    let firstChar = part.charAt(0).toUpperCase();
+
+    let restOfPart = part.slice(1).toLowerCase();
+
+    let formattedPart = firstChar + restOfPart;
+
+    formattedParts.push(formattedPart);
 }
 
-module.exports = formatName;
+let formattedName = formattedParts.join(' ');
+  return formattedName;
+}
+console.log(formatName("  bolu blue   "));
--- test.js
+++
@@ -1,8 +0,0 @@
-const formatThousand = require('./formatThousand');
-const formatName = require('./formatName');
-
-//  formatThousand
-console.log(formatThousand(2000000)); 
-
-// formatName
-console.log(formatName('   bola blue   '));
